98f02c262c43c44650931ef28074aebc799e4bb0
0081998 <0081998@OBD-LAB441.ifmg.edu.br>
2025-11-06 14:04:04 -0300

    Primeira versão com inclusão web e banco de dados

---
+++ src/main/java/model/SistemLanchonete.java
@@ -0,0 +1,11 @@
+package model;
+
+//a implementação do banco de dados vai ser para clientes, enquanto as comidas e ingredientes ficam aqui
+
+import java.util.ArrayList;
+
+
+public class SistemLanchonete {
+    //fazer banco de dados para cadastro de clientes no Individual do pgAdmin amanhã
+    
+}
