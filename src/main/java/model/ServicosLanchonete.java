d9bc6a874b55841c85c75fc34e6b7aa3e3b38bca
Cliente <Cliente@DESKTOP-35J2MJH>
2025-11-08 11:51:18 -0300

    versao com inicio da implementação de banco de dados e dependencias atualizadas

--- src/main/java/model/ServicosLanchonete.java
+++ src/main/java/model/ServicosLanchonete.java
@@ -1,7 +1,5 @@
 package model;
 
-//a implementação do banco de dados vai ser para clientes, enquanto as comidas e ingredientes ficam aqui
-
 import control.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,7 +13,8 @@ import javax.swing.JOptionPane;
 
 
 public class ServicosLanchonete {
-    //fazer banco de dados para cadastro de clientes no Individual do pgAdmin amanhã
+    
+    //cadastro de funcionarios para o banco de dados
     public int cadastrarFuncionarios(DadosFuncionarios dados){
         try {
             Conexao c = new Conexao();
@@ -45,10 +44,116 @@ public class ServicosLanchonete {
         return 0;
     }
     
+    //listagem de funcionarios
     public List<DadosFuncionarios> listaFuncionarios(){
-        List<DadosFuncionarios> retorno;
-        Conexao c = new Conexao();
-        Connection con = c.obterConexao();
-        String SQL = "SELECT * FROM sistemalanchonete.alunos ORDER BY id DESC";
+        try {
+            List<DadosFuncionarios> retorno = new ArrayList<>();
+            Conexao c = new Conexao();
+            Connection con = c.obterConexao();
+            String SQL = "SELECT * FROM sistemalanchonete.funcionarios ORDER BY id DESC";
+            PreparedStatement p = con.prepareStatement(SQL);
+            
+            ResultSet r = p.executeQuery();
+            while (r.next()) {
+                    DadosFuncionarios dados = new DadosFuncionarios();
+                    dados.setNome(r.getString("nome"));
+                    dados.setCpf(r.getString("cpf"));
+                    dados.setTelefone(r.getString("telefone"));
+                    dados.setEndereco(r.getString("endereco"));
+                    dados.setCargo(r.getString("cargo"));
+                    dados.setSalario(r.getDouble("salario"));
+                    dados.setEmail(r.getString("email"));
+                    retorno.add(dados);
+            }
+            con.close();
+            return retorno;
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro na conexão", "ERRO!", JOptionPane.ERROR_MESSAGE);
+            System.getLogger(ServicosLanchonete.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+        return null;
+    }
+    
+    //método para excluir funcionarios
+    public void deletarFuncionario(int idFunc){
+        if(idFunc > 0){
+            try {
+                Conexao c = new Conexao();
+                Connection con = c.obterConexao();
+                String SQL = "DELETE FROM sistemalanchonete.funcionarios WHERE id=?";
+                PreparedStatement p = con.prepareStatement(SQL);
+                
+                p.setInt(1, idFunc);
+                p.execute();
+                con.close();
+                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Erro na conexão", "ERRO!", JOptionPane.ERROR_MESSAGE);
+                System.getLogger(ServicosLanchonete.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
+    }
+    
+    //permite pesquisar com base no Id do funcionario
+    public DadosFuncionarios consultarPorId(int idFunc) {
+        try {
+            DadosFuncionarios retorno = new DadosFuncionarios();
+            Conexao c = new Conexao();
+            Connection con = c.obterConexao();
+            String SQL = "SELECT * FROM sistemalanchonete.funcionarios WHERE id = ?";
+            PreparedStatement p = con.prepareStatement(SQL);
+            
+            p.setInt(1, idFunc);
+            ResultSet r = p.executeQuery();
+            
+            if (r.next()) {
+                retorno.setId(r.getInt("id"));
+                retorno.setNome(r.getString("nome"));
+                retorno.setCpf(r.getString("cpf"));
+                retorno.setTelefone(r.getString("telefone"));
+                retorno.setEndereco(r.getString("endereco"));
+                retorno.setCargo(r.getString("cargo"));
+                retorno.setSalario(r.getDouble("salario"));
+                retorno.setEmail(r.getString("email"));
+            }
+            
+            con.close();
+            return retorno;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro na conexão", "ERRO!", JOptionPane.ERROR_MESSAGE);
+            Logger.getLogger(ServicosLanchonete.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    //arrumar o comando sql e o preparedstatement
+    public void atualizarDadosAluno(DadosFuncionarios dados) {
+        try {
+            Conexao c = new Conexao();
+            Connection con = c.obterConexao();
+            String SQL = "UPDATE sistemalanchonete.funcionarios "
+                    + " set nome = ?, cpf = ?, telefone = ?,"
+                    + " endereço = ?, cargo = ?, salario = ?,"
+                    + " email = ? WHERE id = ?";
+            PreparedStatement p = con.prepareStatement(SQL);
+            
+            p.setString(1, dados.getNome());
+            p.setString(2, dados.getCpf());
+            p.setString(3, dados.getTelefone());
+            p.setString(4, dados.getEndereco());
+            p.setString(5, dados.getCargo());
+            p.setDouble(6, dados.getSalario());
+            p.setString(7, dados.getEmail());
+            
+            p.setInt(9, dados.getId());
+            
+            ResultSet r = p.executeQuery();
+            con.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro na conexão", "ERRO!", JOptionPane.ERROR_MESSAGE);
+            Logger.getLogger(ServicosLanchonete.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
